16d872f8f6b4bccc2cbf0f7ef0717634e30be9ae
rsefraou <rita.sefraouitahiri@unil.ch>
2019-11-25 02:26:04 +0100

    Put ideas of supervised learning

---
+++ report/supervisedlearningideasMakeupAlley .R
@@ -0,0 +1,12 @@
+#Supervised learning 
+
+#ideas 
+
+#What grade with what emotion ?
+#with negative/ positive or all emotions?
+
+#What brand?
+#With number of words ? with Age ?
+
+#What product ?
+#With number of words ? with Age ?
