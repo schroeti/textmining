8a7d62f75383423ab16d4bf0f335e72386260228
rsefraou <rita.sefraouitahiri@unil.ch>
2019-12-09 18:08:59 +0100

    Add code for report
    Analysis to keeo

--- report/CodeMakeupAlleyRapport.R
+++ report/CodeMakeupAlleyRapport.R
@@ -1,4 +1,3 @@
-
 library(caret)
 library(data.table)
 library(ggforce)
@@ -11,12 +10,16 @@ library(quanteda)
 library(RColorBrewer)
 library(readr)
 library(RSentiment)
+library(rword2vec)
 library(sentimentr)
 library(sunscReen)
 library(textstem)
+library(text2vec)
 library(tidytext)
 library(tidyverse)
+library(topicmodels)
 library(wordcloud)
+
 
 
 #Loading the data set. Make sure you are in the right working repository 
@@ -59,7 +62,7 @@ sunscreen$productName<-tolower(sunscreen$productName)
 
 #We put as stopwords common english words as well as the names of the brands and products 
 my_stop_words <- c(word = c("#", "s", "ve", "re", "skin", "sunscreen", "product","spf", brands, productnames, "shield",
-                                 "sunscreen","sunscreens","t","it","It","use"), c(stopwords::stopwords("en")))
+                            "sunscreen","sunscreens","t","it","It","use"), c(stopwords::stopwords("en")))
 
 #We create a review2 column that remains untouched and we tokenize review
 #We then take away the stop words
@@ -71,11 +74,6 @@ sunscreen_cleaned <- sunscreen %>%
   anti_join(stop_words, by = "word") %>%
   anti_join(my_stop_words, by = "word") %>%
   filter(is.na(as.numeric(word)))
-
-#we look at a general wordcloud
-# sunscreen_cleaned %>%
-#   count(word) %>%
-#   with(wordcloud(word, n, max.words = 50, colors=brewer.pal(8, "Spectral")))
 
 #tf-idf: we want to see if some words look specific to a review or to a product. 
 tf_byreview<- sunscreen_cleaned %>%
@@ -100,50 +98,11 @@ tfidf_product<-tf %>%
 #tfidf maximum would be : 
 
 
-#in order to perform a sentiment analysis, we use quanteda to do a corpus : by brand
-# crude.cp_bybrand<-corpus(sunscreen%>%group_by(productId), docid_field = "brandName", text_field = "review")
-# #summary(crude.cp_bybrand)
-# 
-# crude.tk_bybrand <- quanteda::tokens(crude.cp_bybrand, remove_numbers=TRUE, remove_punct=TRUE, remove_symbols=TRUE, remove_separators=TRUE)
-# #crude.tk_bybrand[[1]]
-# 
-# crude.tk_bybrand <- tokens_remove(crude.tk_bybrand, c("sunscreen","skin","product","t","s","quot",stopwords("english")))
-# crude.tk_bybrand <- tokens_remove(crude.tk_bybrand, my_stop_words)
-# 
-# #crude.tk_bybrand[[1]]
-# 
-# crude.dfm <- dfm(crude.tk_bybrand)
-# #View(crude.dfm)
-# 
-# crude.tfidf <- dfm_tfidf(crude.dfm)
-# View(crude.tfidf)
-# 
-
-
-#by product:
-
-# crude.cp_byproduct <-corpus(sunscreen%>%group_by(productId), docid_field = "productName", text_field = "review")
-# summary(crude.cp_byproduct)
-# 
-# crude.tk_byproduct <- quanteda::tokens(crude.cp_bybrand, remove_numbers=TRUE, remove_punct=TRUE, remove_symbols=TRUE, remove_separators=TRUE)
-# crude.tk_byproduct[[1]]
-# 
-# crude.tk_byproduct <- tokens_remove(crude.tk_bybrand, my_stop_words)
-# crude.tk_byproduct <- tokens_remove(crude.tk_bybrand, c("sunscreen","skin","product","t","s","quot",stopwords("english")))
-# 
-# crude.tk_byproduct[[1]]
-# 
-# crude.dfm <- dfm(crude.tk_byproduct)
-# View(crude.dfm)
-# 
-# crude.tfidf <- dfm_tfidf(crude.dfm)
-# View(crude.tfidf)
-
-#Sentiments
+#Sentiments wuth valence shifters
 
 for (i in c(1:length(sunscreen$review))){
-sunscreen$sentiments[i]<-sentiment_by(sunscreen$review[i],
-                                      hash_valence_shifters)
+  sunscreen$sentiments[i]<-sentiment_by(sunscreen$review[i],
+                                        hash_valence_shifters)
 }
 
 sunscreen$sentiments<-sunscreen$sentiments%>%as.numeric()
@@ -175,13 +134,10 @@ spf<-str_extract_all(sunscreen$productName, "\\d+")%>%
 sunscreen<-cbind(sunscreen, spf)
 
 par(mfrow=c(1,1))
-#wordcloud
-# sunscreen_cleaned %>%
-#   count(word) %>%
-#   with(wordcloud(word, n, max.words = 20, colors=brewer.pal(8, "Spectral")))
+
 
 #Try per brand
-# 
+
 my_stop_words<-as_tibble(my_stop_words)
 my_stop_words$word<-my_stop_words$value
 
@@ -193,41 +149,6 @@ sunscreen_cleaned_for_wordcloud <- sunscreen %>%
   anti_join(stop_words, by = "word") %>%
   anti_join(my_stop_words, by = "word") %>%
   dplyr::filter(is.na(as.numeric(word)))
-# 
-# par(mfrow=c(3,3))
-# 
-# for(i in brands) {
-#   sunscreen_cleaned_for_wordcloud %>%
-#     dplyr::select(brandName, word)%>%
-#     filter(brandName == i )%>%
-#     count(word) %>%
-#     with(wordcloud(word, n, max.words = 100, colors=brewer.pal(8, "Spectral")))
-# }
-
-#We see some brands are more associated with makeup, with "primer", "makeup", others with
-#ingredients like "zinc" , "niacinamide" .
-#Some are more oily than others..
-
-#Per product: 
-# 
-# sunscreen_cleaned_for_wordcloudperproduct <- sunscreen %>%
-#   dplyr::mutate(review2 = review) %>%
-#   dplyr::group_by(productName)%>%
-#   as.tibble() %>%
-#   tidytext::unnest_tokens(word, review) %>%
-#   anti_join(stop_words, by = "word") %>%
-#   anti_join(my_stop_words, by = "word") %>%
-#   dplyr::filter(is.na(as.numeric(word)))
-# 
-# par(mfrow=c(3,3))
-# 
-# for(i in productnames%>%c()) {
-#   sunscreen_cleaned_for_wordcloud %>%
-#     dplyr::select(productName, word)%>%
-#     filter(productName == i )%>%
-#     count(word) %>%
-#     with(wordcloud(word, n, max.words = 100, colors=brewer.pal(8, "Spectral")))
-# }
 
 #Sentiment analysis by brand
 
@@ -368,8 +289,10 @@ plot_sentiment_page_sun_product(1)%>%plot()
 plot_sentiment_pagenormalized_sun_product(1)%>%plot()
 
 
+##SIMILARITIES:
 
-###
+##STILL WANT TO ADD A TAG FOR BRAND / PRODUCT 
+
 sunscreen_nostopword_corpus <- sunscreen %>%
   mutate(review2 = review) %>%
   as.tibble()
@@ -399,104 +322,25 @@ sunscreen_corpus_perbrand<-corpus(sunscreen_nostopword_corpusbrand$review)%>%
 sunscreen_corpus<- tokens_remove(sunscreen_corpus, pattern= my_stop_words, padding=FALSE)
 sunscreen_corpus_perbrand<- tokens_remove(sunscreen_corpus_perbrand, pattern= my_stop_words, padding=FALSE)
 
-## 1. method = jaccard
-
-# summary(sunscreen_corpus)
-# summary(sunscreen_nostopword_corpusbrand)
- dfmat <- dfm(sunscreen_corpus,
-              remove_punct = TRUE, remove = stopwords("english"))
-# View(dfmat)
-# dfmat[,1:5]
-# (tstat1 <- textstat_simil(dfmat, method = "jaccard", margin = "documents"))
-# 
-# sum((dfmat[1,] > 0) & (dfmat[2,] > 0)) # common words=25
-# sum((dfmat[1,] > 0) | (dfmat[2,] > 0)) # used words (unique)=221
-# tstat1[1,2]
-# ## 33/220  = tstat1[1,2]
-# tstat1[20,30]
-
-## 2. method = cosine
+ method = cosine
 (tstat2 <- textstat_simil(dfmat, method = "cosine", margin = "documents"))
 sum(dfmat[1,]*dfmat[2,])/sqrt(sum(dfmat[1,]^2)*sum(dfmat[2,]^2))
 
 
 par(mfrow=c(1,1))
+
 plot(hclust(as.dist(1-tstat2)))
-##ADD COLOR BY PRODUCT AND BRAND
-
-## 3. For features (i.e. tokens or words)
-# (tstat3 <- textstat_simil(dfmat, method = "jaccard", margin = "features"))
-# as.matrix(tstat3)[1:2,1:2]
-# dfmat[,1:2]
-
-## 4. with tf-idf
-# (tstat4 <- textstat_simil(dfm_tfidf(dfmat), method = "jaccard", margin = "documents"))
 
 
-
-## LSA
-## Running the LSA with quanteda
 dfmat <- dfm(sunscreen_corpus,
-             remove_punct = TRUE, remove = stopwords("english"))
+             remove_punct = TRUE, tolower=TRUE, remove = stopwords("english"))
 dfmat<-dfm(sunscreen_corpus,
            remove_punct = TRUE, remove = my_stop_words$word)
-tmod <- textmodel_lsa(dfmat, nd=58) # see also nd=58, 57, 10 etc.
-head(tmod$docs)
-head(tmod$features)
 
-## some graphical representation
-library(ggplot2)
-df.doc <- data.frame(dim1=tmod$docs[,1], dim2=tmod$docs[,2])
-rownames(df.doc) <- rownames(tmod$docs)
-ggplot(df.doc, aes(x=dim1, y=dim2)) +
-  geom_point() +
-  geom_text(label=rownames(df.doc))
-
-df.feat <- data.frame(dim1=tmod$features[,1], dim2=tmod$features[,2])
-rownames(df.feat) <- rownames(tmod$features)
-ggplot(df.feat, aes(x=dim1, y=dim2)) +
-  geom_point() +
-  geom_text(label=rownames(df.feat))
-
-## Low rank matrix calculations
-dfmat.test <- tmod$docs %*% diag(tmod$sk) %*% t(tmod$features)
-range(dfmat.test - dfmat)
-range(dfmat.test - tmod$matrix_low_rank )
-
-## Look at the terms linked to topic 1 and 2
-library(dplyr)
-lsa.terms.tbl <- tibble(Term=rownames(tmod$features), topic1=tmod$features[,1],
-                        topic2=tmod$features[,2])
-
-topic1.tbl <- rbind(top_n(lsa.terms.tbl, wt=topic1, n=10), top_n(lsa.terms.tbl, wt=topic1, n=-10))
-ggplot(topic1.tbl, aes(x = reorder(Term, -topic1), y = topic1))+
-  geom_bar(stat="identity", color='skyblue',fill='steelblue') +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
-
-topic2.tbl <- rbind(top_n(lsa.terms.tbl, wt=topic2, n=10), top_n(lsa.terms.tbl, wt=topic2, n=-10))
-ggplot(topic2.tbl, aes(x = reorder(Term, -topic2), y = topic2))+
-  geom_bar(stat="identity", color='skyblue',fill='steelblue') +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
-
-## Look at the documents linked to topic 1 and 2
-lsa.doc.tbl <- tibble(Doc=rownames(tmod$docs), topic1=tmod$docs[,1],
-                      topic2=tmod$docs[,2])
-
-topic1.tbl <- rbind(top_n(lsa.doc.tbl, wt=topic1, n=10), top_n(lsa.doc.tbl, wt=topic1, n=-10))
-ggplot(topic1.tbl, aes(x = reorder(Doc, -topic1), y = topic1))+
-  geom_bar(stat="identity", color='skyblue',fill='steelblue') +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
-
-topic2.tbl <- rbind(top_n(lsa.doc.tbl, wt=topic2, n=10), top_n(lsa.doc.tbl, wt=topic2, n=-10))
-ggplot(topic2.tbl, aes(x = reorder(Doc, -topic2), y = topic2))+
-  geom_bar(stat="identity", color='skyblue',fill='steelblue') +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
+#LDA
 
 
-## ###########################
-## LDA
 
-library(topicmodels)
 ## convert quateda object to topicmodels object
 dtm <- convert(dfmat, to = "topicmodels")
 lda <- LDA(dtm, k = 10) # build 10 topics
@@ -508,14 +352,11 @@ lda@beta[,1:10] # 1 first words
 lda@gamma
 
 ## show the betas of each document
-library(tidytext)
-library(ggplot2)
-library(dplyr)
+
 beta.td <- tidy(lda, matrix = "beta")
 beta.td
 
 filter(beta.td, topic==1) ## all for topic 1
-sum(filter(beta.td, topic==1)$beta)
 
 ## describes the topics with their most associated terms
 beta.top.terms <- beta.td %>%
@@ -534,9 +375,6 @@ beta.top.terms %>%
 
 ## describes the topics in each documents
 gamma.td <- tidy(lda, matrix = "gamma")
-gamma.td
-filter(gamma.td, document=="1825-Adams") ## topics in this documents
-sum(filter(gamma.td, document=="1825-Adams")$gamma)
 
 gamma.td %>%
   ggplot(aes(document, gamma, fill = factor(topic))) +
@@ -548,81 +386,43 @@ gamma.td %>%
 ## Assignment of topic to term in each document
 augment(lda)
 
-## New documents
-lda2 <- LDA(dtm[-1,], k = 10) # set asside 1 document
-posterior(lda2, newdata = dtm[1,])$topics
+### WORD EMBEDDING :
 
 
+#GloVe
 
-##----------------------------------------------------- LDA
+sunscreen.fcm <- fcm(sunscreen_corpus, 
+                     context = "window",
+                     count = "weighted",
+                     weights = 1/(1:5),
+                     tri = TRUE)
 
-library(topicmodels)
-## convert quateda object to topicmodels object
-dtm <- convert(dfmat, to = "topicmodels")
-lda <- LDA(dtm, k = 10) # build 10 topics
-terms(lda, 5) # see the 5 terms most associated with each topic
-topics(lda, 5) # see the 5 topics most associated with each documents
+get_similar <- function(w, df, k = 10) {
+  if(is.character(w)) {
+    v <- as.numeric(df[w,])
+  } else {
+    v <- as.numeric(w)
+  }
+  nv <- sqrt(sum(v^2))
+  m <- as.matrix(df)
+  rownorms <- sqrt(rowSums(m^2))
+  sims <- m %*% v / (nv * rownorms)
+  names(sims) <- rownames(df)
+  return(head(sort(sims, decreasing = TRUE), n = k))
+}
 
-## Extract the beta and gamma from topicmodels object
-lda@beta[,1:10] # 1 first words
-lda@gamma 
+glove <- GlobalVectors$new(word_vectors_size = 25, vocabulary = featnames(sunscreen.fcm), x_max = 10)
+sunscreen.glove <- as.dfm(fit_transform(sunscreen.fcm, glove, n_iter = 10) + t(glove$components))
 
-## show the betas of each document
-library(tidytext)
-library(ggplot2)
-library(dplyr)
-beta.td <- tidy(lda, matrix = "beta")
-beta.td
+get_similar(sunscreen.glove["lotion",] - sunscreen.glove["liquid",] + sunscreen.glove["cream",], sunscreen.glove)
 
-filter(beta.td, topic==1) ## all for topic 1
-sum(filter(beta.td, topic==1)$beta)
-
-## describes the topics with their most associated terms
-beta.top.terms <- beta.td %>%
-  group_by(topic) %>%
-  top_n(10, beta) %>%
-  ungroup() %>%
-  arrange(topic, -beta)
-
-beta.top.terms %>%
-  mutate(term = reorder_within(term, beta, topic)) %>%
-  ggplot(aes(term, beta, fill = factor(topic))) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~ topic, scales = "free") +
-  coord_flip() +
-  scale_x_reordered()
-
-## describes the topics in each documents
-gamma.td <- tidy(lda, matrix = "gamma")
-gamma.td
-filter(gamma.td, document=="") ## topics in this documents
-sum(filter(gamma.td, document=="")$gamma) 
-
-gamma.td %>%
-  ggplot(aes(document, gamma, fill = factor(topic))) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~ topic, scales = "free") +
-  coord_flip() +
-  scale_x_reordered()
-
-## Assignment of topic to term in each document
-augment(lda)
-
-## New documents
-lda2 <- LDA(dtm[-1,], k = 10) # set asside 1 document
-posterior(lda2, newdata = dtm[1,])$topics
-
-#--------------------------------------------------------------#
-## Boxplot
-boxplot(dtotal$comment_score, xlab="Traffic following a post")
-
-
-#que les positifs et on enlève les outliers
-dtotal_pos <- dtotal %>% 
-  filter(dtotal$comment_score < 10 & dtotal$comment_score > 0)
-
-boxplot(dtotal_pos$comment_score, xlab="Number of interactions")
-
-
-
+top50 <- names(get_similar("grail", sunscreen.glove, 50))[2:50]
+distances <- textstat_dist(sunscreen.glove[top50,])
+clustering <- hclust(as.dist(distances))
+cluster.assignments = cutree(clustering, k = 3)
+plot(clustering)
+#pca 
+pca <- prcomp(sunscreen.glove[top50,])
+plot(pca$x[,1:2], col='white')
+text(pca$x[,1:2], labels = rownames(pca$x))
 
